ea246716d03faddf1d4a3593556d93cf9c003528
Yassine Boumahdi <yassineboubou62@gmail.com>
2026-06-19 23:30:49 +0100

    Change WhatsApp chat ID format for alerts

--- portal-scraper/checkmarks.js
+++ portal-scraper/checkmarks.js
@@ -21,7 +21,7 @@ const CACHE_FILE = path.join(__dirname, 'marks_cache.json');
 async function sendWhatsAppAlert(message) {
   const url = `https://api.green-api.com/waInstance${GREEN_API_INSTANCE_ID}/sendMessage/${GREEN_API_TOKEN_INSTANCE}`;
   const payload = {
-    chatId: `${TARGET_PHONE_NUMBER}@c.us`,
+    chatId: `${TARGET_PHONE_NUMBER}@g.us`,
     message: message
   };
 
